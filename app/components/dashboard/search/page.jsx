a923de5d04f2ac92d73d22546e653ee423fefd69
Johansen <Johamaster@gmail.com>
2024-10-17 13:22:49 -0400

    Error en el search

--- app/components/dashboard/search/page.jsx
+++ app/components/dashboard/search/page.jsx
@@ -1,12 +1,23 @@
 "use client"
+import PropTypes from 'prop-types';
 import styles from "./search.module.css"
 import { MdSearch } from "react-icons/md"
 
-const Search = ({placeholder}) => {
-    return <div className={styles.container}>
-        <MdSearch/>
-        <input type="text" placeholder={placeholder} className={styles.input}/>
-    </div>
+const Search = ({ placeholder }) => {
+    return (
+        <div className={styles.container}>
+            <MdSearch />
+            <input 
+                type="text" 
+                placeholder={placeholder} 
+                className={styles.input} 
+            />
+        </div>
+    );
 }
 
-export default Search
+Search.propTypes = {
+    placeholder: PropTypes.string.isRequired,
+};
+
+export default Search;
